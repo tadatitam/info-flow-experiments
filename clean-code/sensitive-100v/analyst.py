1477e2719a01a1491222bc7b8aa35f090c2d5710
tadatitam <amitdatta49@gmail.com>
2014-04-21 19:39:23 -0400

    added sensitive100v

--- clean-code/sensitive-100v/analyst.py
+++ clean-code/sensitive-100v/analyst.py
@@ -16,7 +16,7 @@ ALEXA_SITES = [	"http://www.alexa.com/topsites/category/Top/Health/Mental_Health
 				]
 # INT_SITE_FILE = "int_"+SITE_FILE
 SHORT_COLLECT_PY = "collector/short/collect.py"
-LOG_FILE = "log.txt"
+# LOG_FILE = "log.txt"
 RUN_COLLECT_PY = "collector/collect/collect.py"
 
 
@@ -29,7 +29,7 @@ if(len(SITE_FILES) != len(ALEXA_SITES)):
 
 for i in range(0, len(SITE_FILES)):
 # 	os.system("python collector/short/alexa.py %s %s" % (SITE_FILES[i], ALEXA_SITES[i]))
-	os.system("python collector/short/shortlist.py %s %s %s " % (SITE_FILES[i], "int_"+SITE_FILES[i], SHORT_COLLECT_PY))
-# 	os.system("python collector/collect/run.py %s %s %s" % (LOG_FILE, SITE_FILE, RUN_COLLECT_PY))
-# 	os.system("python tester/pilot.py %s > outb" % (LOG_FILE))
-# 	os.system("python tester/test.py %s > outc" % (LOG_FILE))
+# 	os.system("python collector/short/shortlist.py %s %s %s " % (SITE_FILES[i], "int_"+SITE_FILES[i], SHORT_COLLECT_PY))
+	os.system("python collector/collect/run.py %s %s %s" % ("log."+SITE_FILE[i], SITE_FILE[i], RUN_COLLECT_PY))
+	os.system("python tester/pilot.py %s > outb" % (LOG_FILE))
+	os.system("python tester/test.py %s > outc" % (LOG_FILE))
