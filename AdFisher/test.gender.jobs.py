1aacef4f63d1facc6a2d5b2e05c3945483672c72
tadatitam <amitdatta49@gmail.com>
2014-05-19 10:32:42 -0400

    added genderjobs experiment

---
+++ AdFisher/test.gender.jobs.py
@@ -0,0 +1,29 @@
+import adfisher
+
+site_file = 'employment.txt'
+log_file = 'log.genjobs.txt'
+
+## Collect sites from alexa
+
+adfisher.collect_sites_from_alexa(nsites=100, output_file=site_file, browser="firefox", 
+	alexa_link="http://www.alexa.com/topsites/category/Top/Business/Employment")
+
+## Set up treatments
+
+treatment1 = adfisher.Treatment("female")
+treatment1.opt_in()
+treatment1.set_gender("female")
+treatment1.visit_sites(site_file)
+
+treatment2 = adfisher.Treatment("male")
+treatment2.opt_in()
+treatment2.set_gender("male")
+treatment2.visit_sites(site_file)
+
+## Run Experiment
+
+adfisher.run_experiment(treatments=[treatment2, treatment1], samples=2, blocks=100, reloads=10, log_file=log_file)
+
+## Analyze Data
+
+adfisher.run_analysis(log_file, verbose=True)
