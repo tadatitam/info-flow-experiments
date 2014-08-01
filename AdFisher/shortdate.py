787de8e0996274f8ce939fa79e2f007e61d7cd74
tadatitam <amitdatta49@gmail.com>
2014-08-01 12:16:57 +0530

    modified shortdate

--- AdFisher/shortdate.py
+++ AdFisher/shortdate.py
@@ -3,6 +3,9 @@ import adfisher
 site_file = 'dating_all.txt'
 log_file = 'log.shortdate.txt'
 
+adfisher.collect_sites_from_alexa(nsites=100, output_file=site_file, browser="firefox", 
+	alexa_link="http://www.alexa.com/topsites/category/Top/Society/Relationships/Dating")
+
 adfisher.shortlist_sites(site_file, target_file="int_"+site_file, timeout=100)
 
 log_file = 'log.dating.remove.txt'
