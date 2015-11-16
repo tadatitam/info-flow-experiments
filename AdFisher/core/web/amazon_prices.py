8a0680431d307e4a477004c3448390663924b4ae
tadatitam <amitdatta49@gmail.com>
2015-11-16 16:40:38 -0500

    added prices files

---
+++ AdFisher/core/web/amazon_prices.py
@@ -0,0 +1,31 @@
+import time, re                                                     # time.sleep, re.split
+import sys                                                          # some prints
+from selenium import webdriver                                      # for running the driver on websites
+from datetime import datetime                                       # for tagging log with datetime
+from selenium.webdriver.common.keys import Keys                     # to press keys on a webpage
+from selenium.webdriver.common.action_chains import ActionChains    # to move mouse over
+# import browser_unit
+import google_ads                                                # interacting with Google Ads
+
+# strip html
+
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()  
+
+class AmazonPricesUnit(google_ads.GoogleAdsUnit):
+
+    def __init__(self, browser, log_file, unit_id, treatment_id, headless=False, proxy=None):
+        google_ads.GoogleAdsUnit.__init__(self, browser, log_file, unit_id, treatment_id, headless, proxy=proxy)
