9887c1db89c50b511d5c0760317854ea90271089
Roy Ragsdale <roy@apexcno.com>
2015-12-01 10:13:06 -0500

    Full featured adblock collection demo
    Renamed demonstration experiment to match convention

--- AdFisher/examples/demo_adblock_experiment.py
+++ AdFisher/examples/demo_adblock_experiment.py
@@ -13,7 +13,7 @@ adblock_rules = web.adblock_ads.AdBlockUnit(log_file=log_file).rules
 # Defines the browser that will be used as a "unit" and gives it a copy of the adblock_rules
 def make_browser(unit_id, treatment_id):
     b = web.adblock_ads.AdBlockUnit(log_file=log_file, unit_id=unit_id, 
-        treatment_id=treatment_id, headless=True,rules=adblock_rules)
+        treatment_id=treatment_id, headless=False,rules=adblock_rules)
     return b
 
 # Control Group treatment (blank)
@@ -28,25 +28,22 @@ def exp_treatment(unit):
 # checks all the sites that adfisher could previously collect on
 # (~10 minutes for src and href)
 def measurement(unit):
-    #unit.collect_ads("http://www.foxnews.com/us/index.html",reloads=3,delay=5)
-    #unit.collect_ads("http://www.bbc.com/news/",reloads=3,delay=5)
-
-    unit.collect_ads("http://www.foxnews.com/us/index.html")
-    unit.collect_ads("http://www.bbc.com/news/")
 
     # from google_ads
-    #unit.collect_ads("http://www.foxnews.com/us/index.html")
-    #unit.collect_ads("http://www.bloomberg.com/")
-    #unit.collect_ads("http://www.reuters.com/news/us")
-    #unit.collect_ads("http://www.theguardian.com/us")
-    #unit.collect_ads("http://timesofindia.indiatimes.com/international-home")
-    #unit.collect_ads("http://www.bbc.com/news/")
-
+    sites = ["http://www.foxnews.com/us/index.html",
+            "http://www.bloomberg.com/",
+            "http://www.reuters.com/news/us",
+            "http://www.theguardian.com/us",
+            "http://timesofindia.indiatimes.com/international-home",
+            "http://www.bbc.com/news/"]
+    
+    for site in sites:
+        unit.collect_ads(site,reloads=2,delay=5)
 
     #from bing_ads
-    #for site in ["news", "weather", "entertainment", "sports", "money",
-    #            "lifestyle", "health", "foodanddrink","travel", "autos"]:
-    #    unit.collect_ads("http://www.msn.com/en-us/"+site)
+    for site in ["news", "weather", "entertainment", "sports", "money",
+                "lifestyle", "health", "foodanddrink","travel", "autos"]:
+        unit.collect_ads("http://www.msn.com/en-us/"+site,reloads=2,delay=5)
 
 # Shuts down the browser once we are done with it.
 def cleanup_browser(unit):
@@ -64,6 +61,6 @@ def test_stat(observed_values, unit_assignments):
 adfisher.do_experiment(make_unit=make_browser, treatments=[control_treatment, exp_treatment], 
                         measurement=measurement, end_unit=cleanup_browser,
                         load_results=load_results, test_stat=test_stat, ml_analysis=False, 
-                        num_blocks=1, num_units=4, timeout=2000,
+                        num_blocks=2, num_units=4, timeout=2000,
                         log_file=log_file, exp_flag=True, analysis_flag=False, 
                         treatment_names=["control", "experimental"])
